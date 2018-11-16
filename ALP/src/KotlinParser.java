a2714b0386e968526ad42498f9d033f275709a46
GaboGomez <gabriel@altomobile.io>
2018-11-15 19:07:30 -0600

    Added class declaration parse (without testing)

--- ALP/src/KotlinParser.java
+++ ALP/src/KotlinParser.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class KotlinParser {
@@ -41,6 +43,53 @@ public class KotlinParser {
         if (currentToken.id == "A" && peekToken(1).type == "class") {
             updateCurrentToken();
             print("class " + currentToken.id, true);
+        }
+
+        while (currentToken.id != "He" || currentToken.id != "She" || currentToken.id != "To") {
+            updateCurrentToken();
+        }
+
+        if (currentToken.id == "To") { return true; }
+
+        // TODO: Check attributes
+        if (peekToken(1).id == "has") {
+            updateCurrentToken();
+            while (currentToken.id != ".") {
+                updateCurrentToken();
+                String attributeName = currentToken.id;
+                String attributeType = currentToken.type;
+                updateCurrentToken();
+                updateCurrentToken();
+
+                String attributeVal = "";
+                if (attributeType == "pair") {
+                    while (currentToken.id != ")") {
+                        attributeVal += currentToken.id;
+                        updateCurrentToken();
+                    }
+                }
+                else {
+                    attributeVal = currentToken.id;
+                }
+
+                print(attributeType + " " + attributeName + " = " + attributeVal, true);
+                updateCurrentToken();
+            }
+
+            updateCurrentToken();
+        }
+
+        if (peekToken(1).id == "can") {
+            updateCurrentToken();
+            HashSet<String> methods = new HashSet<>();
+            while (currentToken.id != ".") {
+                updateCurrentToken();
+
+                String methodName = currentToken.id;
+                methods.add(methodName);
+
+                updateCurrentToken();
+            }
         }
 
         return true;
